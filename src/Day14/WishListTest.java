9fd668d08b39d9704e55f0db03715011b77c16ad
Tunahan <tunahanboyaci1@gmail.com>
2023-09-25 14:30:04 -0400

    Random Wishlist Item Validation Test

--- src/Day14/WishListTest.java
+++ src/Day14/WishListTest.java
@@ -3,8 +3,6 @@ package Day14;
 import Utilities.PageObjectModel;
 import Utilities.ParameterDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
